35d65bab4439d00b3002b70f5ac001fe55de482a
paodeveloper02-ops <paodeveloper02@gmail.com>
2026-04-02 18:20:00 -0300

    Se agrega variables.py

---
+++ variables.py
@@ -0,0 +1,9 @@
+#Programa: Información personal
+#Guardar los datos en variables
+nombre = "Paola"
+edad = 23
+pais = "Uruguay"
+frase_favorita="Nunca dejes de aprender"
+#Mostramos la informacion en consola
+print(nombre)
+
